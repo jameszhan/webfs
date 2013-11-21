cb5280cef97f0611c7d805b962c05ab52ca758d2
James Zhan <zhiqiangzhan@gmail.com>
2013-11-21 17:19:13 +0800

    revise the version.

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'bootstrap-sass-rails'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
+gem 'tire'
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- app/models/inode.rb
+++ app/models/inode.rb
@@ -1,2 +1,13 @@
 class Inode < ActiveRecord::Base
+  include Tire::Model::Search
+  include Tire::Model::Callbacks
+  
+  mapping do
+    indexes :id,            :index    => :not_analyzed
+    indexes :shasum,        :analyzer => :not_analyzed
+    indexes :content_type,  :analyzer => 'snowball'
+    indexes :content_size,  :as       => 'content.size'
+    indexes :author,        :analyzer => 'keyword'
+    indexes :published_on,  :type => 'date', :include_in_all => false
+  end
 end
